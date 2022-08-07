532055f24d32b57c0f2ff91fead0fb8baf2599b2
juansierra02 <103540940+juansierra02@users.noreply.github.com>
2022-08-07 02:20:37 -0300

    .

---
+++ index.js
@@ -0,0 +1,9 @@
+//import express from "express";
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () =>{
+    console.log(`Servidor correiendo en el Puerto: ${PORT}`);
+});
