426574418a568ef4117285a471fc677b4a6dec0c
RUBEN MONTES <rubenmontes58@gmail.com>
2022-08-17 14:18:20 -0300

    falta en frontend

--- app.js
+++ app.js
@@ -1,86 +1,53 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
-const morgan = require('morgan');
-app.use(morgan('dev'));
-const hbs = require('hbs');
-const path = require('path');
-path.join(__dirname, 'views');
-app.set('view engine', 'hbs');
+
+
+var indexRouter = require('./routes/router-home');
+
+
+
+
+
+var app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-hbs.registerPartials(path.join(__dirname, '/views/partials'));
+app.set('view engine', 'hbs');
 
-const port = process.env.PORT || 3000;
-const mysql = require('mysql2');
-const routerHome = require('./routes/router-home');
-const { all } = require('./routes/router-home');
-
-
-
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-connection.connect();
-console.log('Base de datos conectada');
-
-//middelwares
-
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-app.use('/', routerHome);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
-}
-);
-
-
-
-
-
-
-
-
-
+module.exports = app;
